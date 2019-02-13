4277a3454fa3067f6d2a58edbef1d49564061818
SandroSD <sdezerio@soulitsolutions.com>
2019-02-13 09:01:29 -0300

    Video 2

--- node_resapi/app.js
+++ node_resapi/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mysql = require('mysql');
 
 //app.use(morgan('short'));
 app.use(morgan('combined'));
@@ -15,6 +16,31 @@ app.get("/users", (req, res) => {
 	const user1 = {firstName: "Stephen", lastName: "Curry"};
 	const user2 = {firstName: "Kevin", lastName: "Durant"};
 	res.json([user1, user2]);
+});
+
+app.get("/user/:id", (req, res) => {
+	console.log("Fetching user with id: "+req.params.id);
+	const connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		database: 'lbta_mysql'
+	})
+
+	const userId = req.params.id;
+	const queryString = "SELECT * FROM users WHERE id = ?";
+	connection.query(queryString, [userId], (err, rows, fields) => {
+		if(err){
+			console.log("Failed to query for users: "+err);
+			res.sendStatus(500);
+			return ;
+		}
+		console.log("I think we fetched users succestufully");
+		const users = rows.map((row)=>{
+			return {firstName: row.first_name, lastName: row.last_name}
+		});
+		res.json(users);
+	});
+	//res.end();
 });
 
 // localhost:3003
