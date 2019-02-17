599136f883737b56384a0e6cbe11b83ab145c659
mbp <nicolasmonzon0@gmail.com>
2019-02-16 23:31:55 -0300

    NodeJS REST API: Super Simple Setup - Express & NPM (Ep2)

--- app.js
+++ app.js
@@ -2,9 +2,41 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mysql = require('mysql')
 
 app.use(morgan('combined'))
 //app.use(morgan('short'))
+
+app.get('/users/:id',(req, res) => {
+    console.log("Fetching user with id: "+ req.params.id)
+
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'maricel17',
+      database: 'nodeJS'
+    })
+
+    const userId = req.params.id
+    const queryString = "SELECT * FROM users WHERE id = ?"
+    connection.query( queryString, [userId],(err, rows, fields) => {
+      if (err){
+        console.log("Failed to query for users:" + err)
+        res.sendStatus(500)
+        return
+        //throw err
+      }
+      
+      console.log("I think we fetched user successfully")
+
+      const users = rows.map((row) => {
+        return {firstName: row.first_name, lastName: row.last_name}
+      })
+
+      res.json(users)
+    })
+    //res.end()
+})
 
 app.get('/users',(req,res) => {
 var user1 = {firstName: "Stephen", lastName:"Curry"}
