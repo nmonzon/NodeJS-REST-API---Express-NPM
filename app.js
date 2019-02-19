5f4ca6e0f9848144b6c06f9440e466a5bb63dad9
mbp <nicolasmonzon0@gmail.com>
2019-02-19 11:01:29 -0300

    NodeJS REST API: Super Simple Setup - Express & NPM (Ep3)

--- app.js
+++ app.js
@@ -4,18 +4,58 @@ const app = express()
 const morgan = require('morgan')
 const mysql = require('mysql')
 
-app.use(morgan('combined'))
-//app.use(morgan('short'))
+
+//pasar info de una pantalla a otra
+const bodyParser = require('body-parser')
+
+//busca una request por mi.
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static('./public'))
+
+//app.use(morgan('combined'))
+app.use(morgan('short'))
+
+app.post('/user_create', (req,res) => {
+  console.log("Trying to create a new user...")
+  console.log("How do we get data from form")
+
+  console.log("First name: " + req.body.create_first_name)
+  const firstName = req.body.create_first_name
+  const lastName = req.body.create_last_name
+
+  const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)"
+  getConnection().query(queryString, [firstName, lastName], (err, results, fields) => {
+    if (err) {
+      console.log("Failed to insert new user: " + err)
+      res.sendStatus(500)
+      return
+    }
+
+    console.log("Inserted a new user with id: ", results.insertedId);
+  })
+
+})
+
+function getConnection(){
+  return mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'maricel17',
+    database: 'nodeJS'
+  })
+}
 
 app.get('/users/:id',(req, res) => {
     console.log("Fetching user with id: "+ req.params.id)
 
-    const connection = mysql.createConnection({
+    /*const connection = mysql.createConnection({
       host: 'localhost',
       user: 'root',
       password: 'maricel17',
       database: 'nodeJS'
-    })
+    })*/
+    const connection = getConnection()
 
     const userId = req.params.id
     const queryString = "SELECT * FROM users WHERE id = ?"
@@ -52,6 +92,6 @@ app.get("/",(req, res) => {
 })
 
 //localhost:3003
-app.listen(3003, () => {
+app.listen(3004, () => {
   console.log("Server is up and listening on 3003...")
 })
