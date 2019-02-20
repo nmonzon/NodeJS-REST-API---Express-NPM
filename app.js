ffb3f80ff75ebceb5fb7ac1c8e3b4878fb9b40c9
mbp <nicolasmonzon0@gmail.com>
2019-02-20 08:53:14 -0300

    NodeJS REST API: Super Simple Setup - Express & NPM (Ep4)

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express()
 const morgan = require('morgan')
 const mysql = require('mysql')
 
+//pasar info de una pantalla a otra
+const bodyParser = require('body-parser')
+
+//busca una request por mi.
+app.use(bodyParser.urlencoded({extended: false}))
 /*router.get('/messages', (req,res) => {
   console.log("show message")
   res.end() 
@@ -12,85 +17,13 @@ const router = require('./routes/user.js')
 
 app.use(router)
 
-//pasar info de una pantalla a otra
-const bodyParser = require('body-parser')
 
-//busca una request por mi.
-app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(express.static('./public'))
 
+
 //app.use(morgan('combined'))
 app.use(morgan('short'))
-
-app.post('/user_create', (req,res) => {
-  console.log("Trying to create a new user...")
-  console.log("How do we get data from form")
-
-  console.log("First name: " + req.body.create_first_name)
-  const firstName = req.body.create_first_name
-  const lastName = req.body.create_last_name
-
-  const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)"
-  getConnection().query(queryString, [firstName, lastName], (err, results, fields) => {
-    if (err) {
-      console.log("Failed to insert new user: " + err)
-      res.sendStatus(500)
-      return
-    }
-
-    console.log("Inserted a new user with id: ", results.insertedId);
-  })
-
-})
-
-function getConnection(){
-  return mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'maricel17',
-    database: 'nodeJS'
-  })
-}
-
-app.get('/users/:id',(req, res) => {
-    console.log("Fetching user with id: "+ req.params.id)
-
-    /*const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'maricel17',
-      database: 'nodeJS'
-    })*/
-    const connection = getConnection()
-
-    const userId = req.params.id
-    const queryString = "SELECT * FROM users WHERE id = ?"
-    connection.query( queryString, [userId],(err, rows, fields) => {
-      if (err){
-        console.log("Failed to query for users:" + err)
-        res.sendStatus(500)
-        return
-        //throw err
-      }
-      
-      console.log("I think we fetched user successfully")
-
-      const users = rows.map((row) => {
-        return {firstName: row.first_name, lastName: row.last_name}
-      })
-
-      res.json(users)
-    })
-    //res.end()
-})
-
-app.get('/users',(req,res) => {
-var user1 = {firstName: "Stephen", lastName:"Curry"}
-const user2 = {firstName: "Kevin", lastName:"Durant"}
-res.json([user1, user2])
-  //res.send("nodemon auto updates when i save the file")
-})
 
 //definimos una accion para la ruta '/'
 app.get("/",(req, res) => {
